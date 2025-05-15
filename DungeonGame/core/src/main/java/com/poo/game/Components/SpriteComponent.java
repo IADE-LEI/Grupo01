62b7b92cee9d61eaeb6a9604a9d294d4f4a1f437
Antonio Neto <antonio.neto@resolucaotecnologica.com>
2025-05-15 22:45:27 +0100

    Merge Color and Image SpriteComponent to base class with overloads

--- DungeonGame/core/src/main/java/com/poo/game/Components/SpriteComponent.java
+++ DungeonGame/core/src/main/java/com/poo/game/Components/SpriteComponent.java
@@ -9,16 +9,40 @@
  */
 package com.poo.game.Components;
 
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 
 public class SpriteComponent extends Component {
+  String imageName;
   Vector2 size;
 
   protected Texture texture;
   protected Sprite sprite;
+
+  public SpriteComponent(Color color, float x, float y, float width, float height) {
+    this.size = new Vector2(width, height);
+    Pixmap pm = new Pixmap(16, 16, Pixmap.Format.RGBA8888);
+    pm.setColor(color);
+    pm.fill();
+    this.texture = new Texture(pm);
+    pm.dispose();
+    this.sprite = new Sprite(texture);
+    this.sprite.setSize(size.x, size.y);
+    this.sprite.setPosition(x, y);
+  }
+
+  public SpriteComponent(String imageName, float x, float y, float width, float height) {
+    this.size = new Vector2(width, height);
+    this.imageName = imageName;
+    this.texture = new Texture("image\\" + imageName);
+    this.sprite = new Sprite(texture);
+    this.sprite.setSize(size.x, size.y);
+    this.sprite.setPosition(x, y);
+  }
 
   public Sprite getSprite() {
     return sprite;
